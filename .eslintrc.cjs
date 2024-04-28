155806544a72faa23704b12d0c65c950eca8f097
brown <brown2243@naver.com>
2024-04-29 00:36:57 +0900

    chore: add eslint-plugin-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,49 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   plugins: [
+    "react",
+    "react-hooks",
     "react-refresh",
     //
     "@typescript-eslint",
-    "react-hooks",
     "import",
     "jsx-a11y",
   ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
     //
-    "eslint-config-prettier",
-    "prettier",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:@tanstack/eslint-plugin-query/recommended",
+    //
+    "prettier",
+    "eslint-config-prettier",
   ],
+  settings: {
+    react: {
+      // version: "detect",
+      version: "18.2",
+    },
+  },
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   rules: {
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+      },
+    ],
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+    "react/no-unknown-property": "off",
+    "react/prop-types": "off",
+
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-unused-vars": "warn",
